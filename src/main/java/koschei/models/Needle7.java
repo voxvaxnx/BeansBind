a90e94f3a451513fe3855dc3b39a637f1a60d842
voxvaxnx <voxvaxnx@yandex.ru>
2023-02-25 19:18:58 +0500

    Закончил работу

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,6 +1,14 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+    private final Deth8 deth;
+
+    public Needle7(Deth8 deth) {
+        this.deth = deth;
+    }
 
     @Override
     public String toString() {
